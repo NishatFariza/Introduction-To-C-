d6998ef084234413a356f23b91b9a290c11e6697
NishatFariza <nishatfarzia78@gmail.com>
2022-10-28 16:31:24 +0600

    variable-literals

---
+++ Introduction-to-CPP/Variables-Literals&constants.cpp
@@ -0,0 +1,9 @@
+// variables
+int age = 14;
+age = 17;
+
+int myNum = 5;  // Integer (whole number without decimals)
+double myFloatNum = 5.50 ; //Floating number with decimals
+char myChar = 'c';          //Character
+string myString = "Hello";   //string(text)
+boolean myBoolean = true ;  //true or false
